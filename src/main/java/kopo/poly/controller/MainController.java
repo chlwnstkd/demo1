8350f6e8c2aa49b7e6c5a8c100491a3f0b9a9582
USER <jun1234755@naver.com>
2023-08-01 22:20:37 +0900

    2023-08-01

--- src/main/java/kopo/poly/controller/MainController.java
+++ src/main/java/kopo/poly/controller/MainController.java
@@ -13,5 +13,25 @@ public class MainController {
         log.info(this.getClass().getName() + ".main 페이지 보여주는 함수 실행");
         return "/main/index";
     }
+    @GetMapping("/main/about")
+    public String about() throws Exception {
+        log.info(this.getClass().getName() + ".about 페이지 보여주는 함수 실행");
+        return "/main/about";
+    }
+    @GetMapping("/main/contact")
+    public String contact() throws Exception {
+        log.info(this.getClass().getName() + ".contact 페이지 보여주는 함수 실행");
+        return "/main/contact";
+    }
+    @GetMapping("/main/shop")
+    public String shop() throws Exception {
+        log.info(this.getClass().getName() + ".shop 페이지 보여주는 함수 실행");
+        return "/main/shop";
+    }
+    @GetMapping("/main/single")
+    public String single() throws Exception {
+        log.info(this.getClass().getName() + ".single 페이지 보여주는 함수 실행");
+        return "/main/single";
+    }
 
 }
